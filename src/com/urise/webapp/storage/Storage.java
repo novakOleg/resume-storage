970da10b211947934c99500e46e510786011afff
Oleg <26novak06oleg93@gmail.com>
2016-07-20 22:29:49 +0300

    Introduce Interface

---
+++ src/com/urise/webapp/storage/Storage.java
@@ -0,0 +1,22 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+/**
+ * Created by 26nov on 20.07.2016.
+ */
+public interface Storage {
+    void clear();
+
+    void update(Resume r);
+
+    void save(Resume r);
+
+    Resume get(String uuid);
+
+    void delete(String uuid);
+
+    Resume[] getAll();
+
+    int size();
+}
